f32cb99f99c9e568f6e3d5b09e4ae9ffc77aa35e
kielkow <matheuskiel@fiorifer.com.br>
2020-01-25 22:09:20 -0300

    Duplicated e-mail

--- __tests__/integration/user.test.js
+++ __tests__/integration/user.test.js
@@ -1,7 +1,13 @@
 import request from 'supertest';
 import app from '../../src/app';
 
+import truncate from '../util/truncate';
+
 describe('', () => {
+  beforeEach(async () => {
+    await truncate();
+  });
+
   it('should be able to register', async () => {
     const response = await request(app)
       .post('/users')
@@ -12,5 +18,25 @@ describe('', () => {
       });
 
     expect(response.body).toHaveProperty('id');
+  });
+
+  it('should not be able to register with duplicated email', async () => {
+    await request(app)
+      .post('/users')
+      .send({
+        name: 'Matheus Kielkowski',
+        email: 'matheuskiel@fiorifer.com.br',
+        password_hash: '123456',
+      });
+
+    const response = await request(app)
+      .post('/users')
+      .send({
+        name: 'Matheus Kielkowski',
+        email: 'matheuskiel@fiorifer.com.br',
+        password_hash: '123456',
+      });
+
+    expect(response.status).toBe(400);
   });
 });
